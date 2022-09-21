8b56d7e07b3945b86cf23eba9ffef41b38abfb00
DavidJC1105 <112865062+DavidJC1105@users.noreply.github.com>
2022-09-21 10:53:08 +0100

    Add files via upload

---
+++ javascriptassignment1.js
@@ -0,0 +1,34 @@
+const PI=3.14;
+
+let r=1;
+
+let l=2;
+
+let w=2;
+
+let a=0.5;
+
+let b=2;
+
+let h=2
+
+let j=1.33;
+
+//multiplication
+
+console.log(PI*r**2);
+
+console.log(2*PI*r);
+
+console.log(w*l);
+
+console.log(a*b*h);
+
+console.log(a**2+b**2);
+
+console.log(l*w*h);
+
+console.log(PI*r**2*h);
+
+console.log(j*PI*r**3);
+
